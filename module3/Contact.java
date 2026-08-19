0a1adb34a591e749cec7fefb13c37d431d5df491
Tegan Martin <teganmartin@gmail.com>
2026-08-19 10:09:46 -0700

    Add Module 3 activity: Contact Manager with HashMap and sorting

---
+++ module3/Contact.java
@@ -0,0 +1,25 @@
+public class Contact{
+
+    private String name;
+    private String phone;
+
+    //this is my constructor
+    public Contact(String name, String phone) {
+        this.name = name;
+        this.phone = phone;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getPhone(){
+        return phone;
+    }
+
+    @Override
+    public String toString(){
+        return name + " | " + phone;
+    }
+
+}
